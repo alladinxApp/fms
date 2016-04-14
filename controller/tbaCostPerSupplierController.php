97942e1c6524c2a09b8f77460efc6d9ac2449941
Rey P. Castanares <alladinx@gmail.com>
2016-04-14 11:45:00 -0700

    commit TBA Cost Per Supplier Report but no export yet..

--- controller/tbaCostPerSupplierController.php
+++ controller/tbaCostPerSupplierController.php
@@ -1,44 +1,108 @@
 <?
-	// SEARCH TBA COST PER SUPPLIER
 	$search = null;
+	$rowData = "";
+	$fromdt = null;
+	$todt = null;
+
 	if(isset($_POST['search']) && !empty($_POST['search']) && $_POST['search'] == 1){
 		$fromdt = date("Y-m-d 00:00");
 		$todt = date("Y-m-d 23:59");
 		
 		if(!empty($_POST['txtFromDt'])){
-			$fromdt = dateFormat($_POST['txtFromDt'], "Y-m-d");
+			$fromdt = dateFormat($_POST['txtFromDt'], "Y-m-d 00:00");
 		}
 
 		if(!empty($_POST['txtToDt'])){
-			$todt = dateFormat($_POST['txtFromDt'], "Y-m-d");
-		}
-		
-		if(!empty($_POST['txtIsWarranty'])){
-			$isWarranty = $_POST['txtIsWarranty'];
-			$search .= "AND isWarranty = '$isWarranty' ";
+			$todt = dateFormat($_POST['txtToDt'], "Y-m-d 23:59");
 		}
 
-		if($search == null){
-			$dt = date("Y-m-d");
-			$search .= "AND (woTransactionDate between '$dt 00:00' AND '$dt 23:59') ";
-		}
+		$total = 0;
+		$totalPercentage = 0;
 
 		// SET FMS DB
 		$fms_db = new DBConfig;
 		$fms_db->setFleetDB();
 
-		// SET WORK ORDER
-		$searchwo = new Table;
-		$searchwo->setSQLType($fms_db->getSQLType());
-		$searchwo->setInstance($fms_db->getInstance());
-		$searchwo->setView("v_workordermaster");
-		$searchwo->setParam("WHERE 1 $search ORDER BY woReferenceNo DESC,woTransactionDate DESC");
-		$searchwo->doQuery("query");
-		$row_searchwo = $searchwo->getLists();
-		$num_searchwo = $searchwo->getLists();
-		
+		// SET SUPPLIER
+		$suppliermst = new Table;
+		$suppliermst->setSQLType($fms_db->getSQLType());
+		$suppliermst->setInstance($fms_db->getInstance());
+		$suppliermst->setView("v_suppliermaster");
+		$suppliermst->setParam("WHERE status = '1' ORDER BY supplierName");
+		$suppliermst->doQuery("query");
+		$row_suppliermst = $suppliermst->getLists();
+
+		for($i=0;$i<count($row_suppliermst);$i++){
+			$supplierID = $row_suppliermst[$i]['supplierID'];
+			$supplierName = $row_suppliermst[$i]['supplierName'];
+
+			$getTotalWO = new Table;
+			$getTotalWO->setSQLType($fms_db->getSQLType());
+			$getTotalWO->setInstance($fms_db->getInstance());
+			$getTotalWO->setCol("SUM(parts) as totalCost");
+			$getTotalWO->setView("v_workordermaster");
+			$getTotalWO->setParam("WHERE supplierID = '$supplierID' AND status = '6' AND (invoiceDate between '$fromdt' AND '$todt')");
+			$getTotalWO->doQuery("query");
+			$row_getTotalWO = $getTotalWO->getLists();
+			$totalCost = $row_getTotalWO[0]['totalCost'];
+
+			$rowData1[] = array("supplierID" => $supplierID
+								,"supplierName" => $supplierName
+								,"totalCost" => $totalCost);
+
+			$total += $totalCost;
+		}
+
+		for($i=0;$i<count($rowData1);$i++){
+			$supplierID = $rowData1[$i]['supplierID'];
+			$supplierName = $rowData1[$i]['supplierName'];
+			$totalAmount = $rowData1[$i]['totalCost'];
+			$percentage = (($totalAmount / $total) * 100);
+			$totalPercentage += $percentage;
+
+			// TOTAL TIRES
+			$getTotalWOD = new Table;
+			$getTotalWOD->setSQLType($fms_db->getSQLType());
+			$getTotalWOD->setInstance($fms_db->getInstance());
+			$getTotalWOD->setView("v_workordermaster 
+				JOIN v_workorderdetail ON v_workorderdetail.woReferenceNo = v_workordermaster.woReferenceNo");
+			$getTotalWOD->setParam("WHERE v_workordermaster.supplierID = '$supplierID' 
+				AND v_workordermaster.status = '6' 
+				AND (v_workordermaster.invoiceDate between '$fromdt' AND '$todt')");
+			$getTotalWOD->doQuery("query");
+			$row_getTotalWOD = $getTotalWOD->getLists();
+
+			$totalTires = 0;
+			$totalBatteries = 0;
+			$totalWipers = 0;
+			$totalMats = 0;
+			$totalTints = 0;
+			for($a=0;$a<count($row_getTotalWOD);$a++){
+				switch($row_getTotalWOD[$a]['partsID']){
+					case "PAR00000001": $totalTires += $row_getTotalWOD[$a]['total']; break; // TIRES
+					case "PAR00000002": $totalBatteries += $row_getTotalWOD[$a]['total']; break; // BATTERIES
+					case "PAR00000003": $totalWipers += $row_getTotalWOD[$a]['total']; break; // WIPERS
+					case "PAR00000004": $totalMats += $row_getTotalWOD[$a]['total']; break; // MATS
+					case "PAR00000005": $totalTints += $row_getTotalWOD[$a]['total']; break; // TINT
+					default: break;
+				}
+			}
+
+			$rowData2[] = array("supplierID" => $supplierID
+								,"supplierName" => $supplierName
+								,"totalAmount" => $totalAmount
+								,"percentage" => $percentage
+								,"totalTires" => $totalTires
+								,"totalBatteries" => $totalBatteries
+								,"totalWipers" => $totalWipers
+								,"totalMats" => $totalMats
+								,"totalTints" => $totalTints
+								);
+		}
+
+		$rowData = $rowData2;
+
 		// CLOSING FMS DB
 		$fms_db->DBClose();
 	}
-	// END SEARCH TBA COST PER SUPPLIER
 ?>
--- view/tba_cost_per_supplier.php
+++ view/tba_cost_per_supplier.php
@@ -9,22 +9,11 @@
                 <div class="form-group">
                     <label class="col-lg-2" for="txtFromDt">From</label>
                     <div class="col-lg-2">
-                        <input type="text" name="txtFromDt" id="txtFromDt" class="form-control gui-input input-sm" placeholder="YYYY-MM-DD" />
+                        <input type="text" name="txtFromDt" value="<?=dateFormat($fromdt,"Y-m-d");?>" id="txtFromDt" class="form-control gui-input input-sm" placeholder="YYYY-MM-DD" />
                     </div>
                     <label class="col-lg-1" for="txtToDt">To</label>
                     <div class="col-lg-2">
-                        <input type="text" name="txtToDt" id="txtToDt" class="form-control gui-input input-sm" placeholder="YYYY-MM-DD" />
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-lg-2" for="txtSupplier">Supplier</label>
-                    <div class="col-lg-2">
-                         <select class="required" name="txtSupplier" id="txtSupplier">
-                            <option value="">Select Supplier</option>
-                            <? for($i=0;$i<count($row_suppliermst);$i++){ ?>
-                            <option value="<?=$row_suppliermst[$i]['supplierID'];?>"><?=$row_suppliermst[$i]['supplierName'];?></option>
-                            <? } ?>
-                        </select>
+                        <input type="text" name="txtToDt" value="<?=dateFormat($todt,"Y-m-d");?>" id="txtToDt" class="form-control gui-input input-sm" placeholder="YYYY-MM-DD" />
                     </div>
                 </div>
                 <div class="form-group">
@@ -38,3 +27,101 @@
         </div>
     </div>
 </div>
+<? if(!empty($rowData)){ ?>
+<form role="form" class="form-horizontal" method="Post" id="workorder-search" target="_blank" action="export.php">
+<div class="row">
+    <div class="form-group">
+        <div class="col-lg-1">
+            <button class="btn btn-sm btn-primary btn-block btn-gradient" type="submit"> EXPORT </button>
+        </div>
+    </div>
+    <input type="hidden" name="export" id="export" value="tba_cost_per_supplier" />
+    <input type="hidden" name="txtFromDt" id="txtFromDt" value="<?=$fromdt;?>" />
+    <input type="hidden" name="txtToDt" id="txtToDt" value="<?=$todt;?>" />
+</div>
+</form>
+<div class="row">
+    <div id="spy8" class="panel">
+        <div class="panel-body pn">
+            <table class="table table-hover table-condensed table-striped table-responsive table-bordered">
+
+                    <tr class="dark">
+                        <th>#</th>
+                        <th>Supplier</th>
+                        <th>Total (%)</th>
+                        <th>Tires</th>
+                        <th>Batteries</th>
+                        <th>Wipers</th>
+                        <th>Mats</th>
+                        <th>Tint</th>
+                    </tr>
+
+                    <? 
+                        $cnt=1; 
+                        for($i=0;$i<count($rowData);$i++){ 
+                            $style = "";
+                            $bg = "background: #fff;";
+
+                            if($cnt % 2){
+                                $bg = "background: #eee;";
+                            }
+
+                            $style = $bg;
+
+                            $totalAmount = $rowData[$i]['totalAmount'];
+                            $totalTires = $rowData[$i]['totalTires'];
+                            $totalBatteries = $rowData[$i]['totalBatteries'];
+                            $totalWipers = $rowData[$i]['totalWipers'];
+                            $totalMats = $rowData[$i]['totalMats'];
+                            $totalTints = $rowData[$i]['totalTints'];
+
+                            $t_totalAmount += $totalAmount;
+                            $t_totalTires += $totalTires;
+                            $t_totalBatteries += $totalBatteries;
+                            $t_totalWipers += $totalWipers;
+                            $t_totalMats += $totalMats;
+                            $t_totalTints += $totalTints;
+                    ?>
+                    <tr>
+                        <td style="<?=$style;?>"><?=$cnt;?></td>
+                        <td style="<?=$style;?>"><?=$rowData[$i]['supplierName'];?></td>
+                        <td align="right" style="<?=$style;?>"><?=number_format($totalAmount,2) . ' (' . $rowData[$i]['percentage'] . '%)';?></td>
+                        <td align="right" style="<?=$style;?>"><?=number_format($totalTires,2);?></td>
+                        <td align="right" style="<?=$style;?>"><?=number_format($totalBatteries,2);?></td>
+                        <td align="right" style="<?=$style;?>"><?=number_format($totalWipers,2);?></td>
+                        <td align="right" style="<?=$style;?>"><?=number_format($totalMats,2);?></td>
+                        <td align="right" style="<?=$style;?>"><?=number_format($totalTints,2);?></td>
+                    </tr>
+                    <? 
+                            $cnt++; 
+                        } 
+                        $noOfSuppliers = ($cnt - 1);
+
+                        $perTires = (($t_totalTires / $t_totalAmount) * 100);
+                        $perBatteries = (($t_totalBatteries / $t_totalAmount) * 100);
+                        $perWipers = (($t_totalWipers / $t_totalAmount) * 100);
+                        $perMats = (($t_totalMats / $t_totalAmount) * 100);
+                        $perTints = (($t_totalTints / $t_totalAmount) * 100);
+                    ?>
+                    <tr class="primary">
+                        <td colspan="2">No of Suppliers: <b><?=$noOfSuppliers;?></b></td>
+                        <td align="right"><?=number_format($t_totalAmount,2);?></td>
+                        <td align="right"><?=number_format($t_totalTires,2);?></td>
+                        <td align="right"><?=number_format($t_totalBatteries,2);?></td>
+                        <td align="right"><?=number_format($t_totalWipers,2);?></td>
+                        <td align="right"><?=number_format($t_totalMats,2);?></td>
+                        <td align="right"><?=number_format($t_totalTints,2);?></td>
+                    </tr>
+                    <tr class="success">
+                        <td colspan="3">&nbsp;</td>
+                        <td align="right"><?=number_format($perTires,2);?>%</td>
+                        <td align="right"><?=number_format($perBatteries,2);?>%</td>
+                        <td align="right"><?=number_format($peripers,2);?>%</td>
+                        <td align="right"><?=number_format($perMats,2);?>%</td>
+                        <td align="right"><?=number_format($perTints,2);?>%</td>
+                    </tr>
+            </table>
+        </div>
+    </div>
+</div>
+<? } ?>
