aaaca19de7766d2a0d1e7e8a395369c623f61ed1
Rey P. Castanares <alladinx@gmail.com>
2016-04-15 08:33:22 -0700

    commit files for repair cost per supplier report

--- controller/repairCostPerSupplierController.php
+++ controller/repairCostPerSupplierController.php
@@ -1,44 +1,145 @@
 <?
-	// SEARCH REPAIR COST PER SUPPLIER
 	$search = null;
+	$rowData = "";
+	$fromdt = null;
+	$todt = null;
+
 	if(isset($_POST['search']) && !empty($_POST['search']) && $_POST['search'] == 1){
 		$fromdt = date("Y-m-d 00:00");
 		$todt = date("Y-m-d 23:59");
 		
 		if(!empty($_POST['txtFromDt'])){
-			$fromdt = dateFormat($_POST['txtFromDt'], "Y-m-d");
+			$fromdt = dateFormat($_POST['txtFromDt'], "Y-m-d 00:00");
 		}
 
 		if(!empty($_POST['txtToDt'])){
-			$todt = dateFormat($_POST['txtFromDt'], "Y-m-d");
-		}
-		
-		if(!empty($_POST['txtIsWarranty'])){
-			$isWarranty = $_POST['txtIsWarranty'];
-			$search .= "AND isWarranty = '$isWarranty' ";
-		}
-
-		if($search == null){
-			$dt = date("Y-m-d");
-			$search .= "AND (woTransactionDate between '$dt 00:00' AND '$dt 23:59') ";
+			$todt = dateFormat($_POST['txtToDt'], "Y-m-d 23:59");
 		}
 
 		// SET FMS DB
 		$fms_db = new DBConfig;
 		$fms_db->setFleetDB();
 
-		// SET WORK ORDER
-		$searchwo = new Table;
-		$searchwo->setSQLType($fms_db->getSQLType());
-		$searchwo->setInstance($fms_db->getInstance());
-		$searchwo->setView("v_workordermaster");
-		$searchwo->setParam("WHERE 1 $search ORDER BY woReferenceNo DESC,woTransactionDate DESC");
-		$searchwo->doQuery("query");
-		$row_searchwo = $searchwo->getLists();
-		$num_searchwo = $searchwo->getLists();
-		
+		// SET WOMST
+		$womst = new Table;
+		$womst->setSQLType($fms_db->getSQLType());
+		$womst->setInstance($fms_db->getInstance());
+		$womst->setView("v_workordermaster");
+		$womst->setParam("WHERE status = '6' AND (invoiceDate BETWEEN '$fromdt' AND '$todt') ORDER BY departmentName");
+		$womst->doQuery("query");
+		$row_womst = $womst->getLists();
+
+		// SET SUPPLIER
+		$suppliermst = new Table;
+		$suppliermst->setSQLType($fms_db->getSQLType());
+		$suppliermst->setInstance($fms_db->getInstance());
+		$suppliermst->setView("v_suppliermaster");
+		$suppliermst->setParam("WHERE status = '1' ORDER BY supplierName");
+		$suppliermst->doQuery("query");
+		$row_suppliermst = $suppliermst->getLists();
+
 		// CLOSING FMS DB
 		$fms_db->DBClose();
+
+		$t_total = 0;
+		$t_average = 0;
+		$t_totalParts = 0;
+		$t_totalMisc = 0;
+		$t_totalOth = 0;
+
+		// LOOP SUPPLIER
+		for($b=0;$b<count($row_suppliermst);$b++){
+			$supplierID = $row_suppliermst[$b]['supplierID'];
+			$supplierName = $row_suppliermst[$b]['supplierName'];
+			$eCnt = 0;
+
+			$total = 0;
+			$totalParts = 0;
+			$totalMisc = 0;
+			$totalLabor = 0;
+			$totalPMS = 0;
+			$totalAC = 0;
+			$totalElec = 0;
+			$totalMech = 0;
+			$totalBody = 0;
+			$totalDetailing = 0;
+			$totalRP = 0;
+			$totalOth = 0;
+
+			// LOOP WORK ORDER MASTER
+			for($z=0;$z<count($row_womst);$z++){
+				$woRefNoMst = $row_womst[$z]['woReferenceNo'];
+				$woSupplierID = $row_womst[$z]['supplierID'];
+				$totalCost = $row_womst[$z]['totalCost'];
+				$parts = $row_womst[$z]['parts'];
+				$miscellaneous = $row_womst[$z]['miscellaneous'];
+				$labor = $row_womst[$z]['labor'];
+				$tax = $row_womst[$z]['tax'];
+				$discount = $row_womst[$z]['discount'];
+				$serviceTypeID = $row_womst[$z]['serviceTypeID'];
+
+				// IF YEAR,MAKE,MODEL EXISTS ON WOMST
+				if($supplierID == $woSupplierID){
+					$eCnt++;
+					$total += $totalCost;
+					$totalParts += $parts;
+					$totalMisc += $miscellaneous;
+					$totalLabor += (($labor + $tax) - $discount);
+
+					// COUNT TOTAL OF EACH SERVICE TYPE
+					switch($serviceTypeID){
+						case "ST002": $totalPMS += $totalCost; break;
+						case "ST003": $totalAC += $totalCost; break;
+						case "ST004": $totalElec += $totalCost; break;
+						case "ST005": $totalMech += $totalCost; break;
+						case "ST006": $totalBody += $totalCost; break;
+						case "ST007": $totalDetailing += $totalCost; break;
+						case "ST008": $totalRP += $totalCost; break;
+						case "ST009": $totalOth += $totalCost; break;
+					}
+				}
+			}
+			// END WORK ORDER MST
+
+			if($eCnt > 0){
+				$average = ($total / $eCnt);
+				$t_total += $total;
+				$t_average += $average;
+				$t_totalParts += $totalParts;
+				$t_totalMisc += $totalMisc;
+				$t_totalLabor += $totalLabor;
+				$t_totalPMS += $totalPMS;
+				$t_totalAC += $totalAC;
+				$t_totalElec += $totalElec;
+				$t_totalMech += $totalMech;
+				$t_totalBody += $totalBody;
+				$t_totalDetailing += $totalDetailing;
+				$t_totalRP += $totalRP;
+				$t_totalOth += $totalOth;
+
+				$rowData1[] = array("supplierID" => $supplierID
+								,"supplierName" => $supplierName
+								,"total" => $t_total
+								,"average" => $t_average
+								,"parts" => $t_totalParts
+								,"miscellaneous" => $t_totalMisc
+								,"labor" => $t_totalLabor
+								,"PMS" => $t_totalPMS
+								,"AC" => $t_totalAC
+								,"elec" => $t_totalElec
+								,"mech" => $t_totalMech
+								,"body" => $t_totalBody
+								,"detailing" => $t_totalDetailing
+								,"RP" => $t_totalRP
+								,"others" => $t_totalOth);
+			}
+			
+			// OVERALL TOTAL
+			$gTotal += $t_total;
+		}
+		// END SUPPLIER
+		
+		$rowData = $rowData1;
 	}
 	// END SEARCH REPAIR COST PER SUPPLIER
 ?>
--- view/repair_cost_per_supplier.php
+++ view/repair_cost_per_supplier.php
@@ -9,22 +9,11 @@
                 <div class="form-group">
                     <label class="col-lg-2" for="txtFromDt">From</label>
                     <div class="col-lg-2">
-                        <input type="text" name="txtFromDt" id="txtFromDt" class="form-control gui-input input-sm" placeholder="YYYY-MM-DD" />
+                        <input type="text" name="txtFromDt" id="txtFromDt" value="<?=dateFormat($fromdt,"Y-m-d");?>" class="form-control gui-input input-sm" placeholder="YYYY-MM-DD" />
                     </div>
                     <label class="col-lg-1" for="txtToDt">To</label>
                     <div class="col-lg-2">
-                        <input type="text" name="txtToDt" id="txtToDt" class="form-control gui-input input-sm" placeholder="YYYY-MM-DD" />
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-lg-2" for="txtSupplier">Supplier</label>
-                    <div class="col-lg-2">
-                         <select class="required" name="txtSupplier" id="txtSupplier">
-                            <option value="">Select Supplier</option>
-                            <? for($i=0;$i<count($row_suppliermst);$i++){ ?>
-                            <option value="<?=$row_suppliermst[$i]['supplierID'];?>"><?=$row_suppliermst[$i]['supplierName'];?></option>
-                            <? } ?>
-                        </select>
+                        <input type="text" name="txtToDt" id="txtToDt" value="<?=dateFormat($todt,"Y-m-d");?>" class="form-control gui-input input-sm" placeholder="YYYY-MM-DD" />
                     </div>
                 </div>
                 <div class="form-group">
@@ -38,3 +27,148 @@
         </div>
     </div>
 </div>
+<? if(!empty($rowData)){ ?>
+<form role="form" class="form-horizontal" method="Post" id="workorder-search" target="_blank" action="export.php">
+<div class="row">
+    <div class="form-group">
+        <div class="col-lg-1">
+            <button class="btn btn-sm btn-primary btn-block btn-gradient" type="submit"> EXPORT </button>
+        </div>
+    </div>
+    <input type="hidden" name="export" id="export" value="repair_cost_per_supplier" />
+    <input type="hidden" name="txtFromDt" id="txtFromDt" value="<?=$fromdt;?>" />
+    <input type="hidden" name="txtToDt" id="txtToDt" value="<?=$todt;?>" />
+</div>
+</form>
+<div class="row">
+    <div id="spy8" class="panel">
+        <div class="panel-body pn">
+            <table class="table table-hover table-condensed table-striped table-responsive table-bordered" style="font-size: 10px;">
+
+                    <tr class="dark">
+                        <th>Supplier</th>
+                        <th>Total ( Average )</th>
+                        <th>Parts</th>
+                        <th>Oil/Lubs</th>
+                        <th>Others</th>
+                        <th colspan="8">Service Type</th>
+                    </tr>
+                    <tr class="dark">
+                        <th colspan="5">&nbsp;</th>
+                        <th>PMS</th>
+                        <th>A/C</th>
+                        <th>Elec</th>
+                        <th>Mech</th>
+                        <th>Body</th>
+                        <th>Detailing</th>
+                        <th>RP</th>
+                        <th>Others</th>
+                    </tr>
+                    <? 
+                        $cnt = 1; 
+                        $grandTotal = 0;
+                        $t_services = 0;
+                        $t_parts = 0;
+                        $t_misc = 0;
+                        $t_labor = 0;
+                        $t_pms = 0;
+                        $t_ac = 0;
+                        $t_elec = 0;
+                        $t_mech = 0;
+                        $t_body = 0;
+                        $t_detailing = 0;
+                        $t_rp = 0;
+                        $t_others = 0;
+                        for($i=0;$i<count($rowData);$i++){
+                            $grandTotal += $rowData[$i]['total'];
+                            $average = 
+                            $t_parts += $rowData[$i]['parts'];
+                            $t_misc += $rowData[$i]['miscellaneous'];
+                            $t_labor += $rowData[$i]['labor'];
+                            $t_pms += $rowData[$i]['PMS'];
+                            $t_ac += $rowData[$i]['AC'];
+                            $t_elec += $rowData[$i]['elec'];
+                            $t_mech += $rowData[$i]['mech'];
+                            $t_body += $rowData[$i]['body'];
+                            $t_detailing += $rowData[$i]['detailing'];
+                            $t_rp += $rowData[$i]['RP'];
+                            $t_others += $rowData[$i]['others'];
+
+                            $bg = null;
+                            
+                            if($cnt % 2){
+                                $bg = 'background: #eee;';
+                            }
+
+                            $style = $bg;
+                    ?>
+                    <tr>
+                        <td <?=$style;?>><?=$cnt;?>.) <?=$rowData[$i]['supplierName'];?></td>
+                        <td <?=$style;?> align="right"><?=number_format($rowData[$i]['total'],2);?> ( <?=number_format($rowData[$i]['average'],2);?> )</td>
+                        <td <?=$style;?> align="right"><?=number_format($rowData[$i]['parts'],2);?></td>
+                        <td <?=$style;?> align="right"><?=number_format($rowData[$i]['miscellaneous'],2);?></td>
+                        <td <?=$style;?> align="right"><?=number_format($rowData[$i]['labor'],2);?></td>
+                        <td <?=$style;?> align="right"><?=number_format($rowData[$i]['PMS'],2);?></td>
+                        <td <?=$style;?> align="right"><?=number_format($rowData[$i]['AC'],2);?></td>
+                        <td <?=$style;?> align="right"><?=number_format($rowData[$i]['elec'],2);?></td>
+                        <td <?=$style;?> align="right"><?=number_format($rowData[$i]['mech'],2);?></td>
+                        <td <?=$style;?> align="right"><?=number_format($rowData[$i]['body'],2);?></td>
+                        <td <?=$style;?> align="right"><?=number_format($rowData[$i]['detailing'],2);?></td>
+                        <td <?=$style;?> align="right"><?=number_format($rowData[$i]['RP'],2);?></td>
+                        <td <?=$style;?> align="right"><?=number_format($rowData[$i]['others'],2);?></td>
+                    </tr>
+                    <? $cnt++; } $noOfSuppliers = ($cnt - 1); ?>
+            </table>
+        </div>
+    </div>
+    <div class="row">
+        <div class="form-group">
+            <div class="col-lg-2">GRAND TOTAL</div>
+            <div class="col-lg-2">: <b><?=number_format($grandTotal,2);?></b></div>
+            <div class="col-lg-6 text-center"><b>SERVICES</b></div>
+            <div class="col-lg-2">&nbsp;</div>
+        </div>
+        <div class="form-group">
+            <div class="col-lg-2">No Of Suppliers</div>
+            <div class="col-lg-2">: <b><?=$noOfSuppliers;?></b></div>
+            <div class="col-lg-1">PMS</div>
+            <div class="col-lg-2">: <b><?=number_format($t_pms,2);?></b></div>
+            <div class="col-lg-1">Body</div>
+            <div class="col-lg-2">: <b><?=number_format($t_body,2);?></b></div>
+            <div class="col-lg-2">&nbsp;</div>
+        </div>
+        <div class="form-group">
+            <div class="col-lg-2">Services</div>
+            <div class="col-lg-2">: <b><? ?></b></div>
+            <div class="col-lg-1">A/C</div>
+            <div class="col-lg-2">: <b><?=number_format($t_ac,2);?></b></div>
+            <div class="col-lg-1">Detailing</div>
+            <div class="col-lg-2">: <b><?=number_format($t_detailing,2);?></b></div>
+            <div class="col-lg-2">&nbsp;</div>
+        </div>
+        <div class="form-group">
+            <div class="col-lg-2">Parts</div>
+            <div class="col-lg-2">: <b><?=number_format($t_parts,2);?></b></div>
+            <div class="col-lg-1">Elec</div>
+            <div class="col-lg-2">: <b><?=number_format($t_elec,2);?></b></div>
+            <div class="col-lg-1">RP</div>
+            <div class="col-lg-2">: <b><?=number_format($t_rp,2);?></b></div>
+            <div class="col-lg-2">&nbsp;</div>
+        </div>
+        <div class="form-group">
+            <div class="col-lg-2">Oil/Lubs</div>
+            <div class="col-lg-2">: <b><?=number_format($t_misc,2);?></b></div>
+            <div class="col-lg-1">Mech</div>
+            <div class="col-lg-2">: <b><?=number_format($t_mech,2);?></b></div>
+            <div class="col-lg-1">Others</div>
+            <div class="col-lg-2">: <b><?=number_format($t_others,2);?></b></div>
+            <div class="col-lg-2">&nbsp;</div>
+        </div>
+        <div class="form-group">
+            <div class="col-lg-2">Misc/Others</div>
+            <div class="col-lg-2">: <b><?=number_format($t_labor,2);?></b></div>
+            <div class="col-lg-8">&nbsp;</div>
+        </div>
+    </div>
+</div>
+<? } ?>
