daf4f2c6b719a36dc35ad5b54cb8423d33662f13
Rey P. Castanares <alladinx@gmail.com>
2016-06-24 00:10:07 +0800

    commit repair cost per supplier report and export

--- export.php
+++ export.php
@@ -642,7 +642,468 @@
 					}
 
 					$data = trim($ln);
-					$filename = "tba_cost_per_supplier_" . $dt . ".csv";
+					$filename = "repair_cost_per_department_" . $dt . ".csv";
+				break;
+			case "repaircostpermake":
+					$fromdt = date("Y-m-d 00:00");
+					$todt = date("Y-m-d 23:59");
+					
+					if(!empty($_POST['txtFromDt'])){
+						$fromdt = dateFormat($_POST['txtFromDt'], "Y-m-d 00:00");
+					}
+
+					if(!empty($_POST['txtToDt'])){
+						$todt = dateFormat($_POST['txtToDt'], "Y-m-d 23:59");
+					}
+
+					// SET FMS DB
+					$fms_db = new DBConfig;
+					$fms_db->setFleetDB();
+
+					// SET WOMST
+					$womst = new Table;
+					$womst->setSQLType($fms_db->getSQLType());
+					$womst->setInstance($fms_db->getInstance());
+					$womst->setView("v_workordermaster");
+					$womst->setParam("WHERE status = '6' AND (invoiceDate BETWEEN '$fromdt' AND '$todt') ORDER BY departmentName");
+					$womst->doQuery("query");
+					$row_womst = $womst->getLists();
+
+					// SET YEAR
+					$yearmst = new Table;
+					$yearmst->setSQLType($fms_db->getSQLType());
+					$yearmst->setInstance($fms_db->getInstance());
+					$yearmst->setView("v_yearmaster");
+					$yearmst->setParam("WHERE status = '1' ORDER BY description");
+					$yearmst->doQuery("query");
+					$row_yearmst = $yearmst->getLists();
+
+					// SET MAKE
+					$makemst = new Table;
+					$makemst->setSQLType($fms_db->getSQLType());
+					$makemst->setInstance($fms_db->getInstance());
+					$makemst->setView("v_makemaster");
+					$makemst->setParam("WHERE status = '1' ORDER BY makeName");
+					$makemst->doQuery("query");
+					$row_makemst = $makemst->getLists();
+
+					// SET MODEL
+					$modelmst = new Table;
+					$modelmst->setSQLType($fms_db->getSQLType());
+					$modelmst->setInstance($fms_db->getInstance());
+					$modelmst->setView("v_modelmaster");
+					$modelmst->setParam("WHERE status = '1' ORDER BY description");
+					$modelmst->doQuery("query");
+					$row_modelmst = $modelmst->getLists();
+
+					// SET EQUIPMENT
+					$equipmentmst = new Table;
+					$equipmentmst->setSQLType($fms_db->getSQLType());
+					$equipmentmst->setInstance($fms_db->getInstance());
+					$equipmentmst->setView("v_equipmentmaster");
+					$equipmentmst->setParam("WHERE status = '1' ORDER BY equipmentID");
+					$equipmentmst->doQuery("query");
+					$row_equipmentmst = $equipmentmst->getLists();
+
+					// CLOSING FMS DB
+					$fms_db->DBClose();
+
+					// LOOP MAKE
+					for($b=0;$b<count($row_makemst);$b++){
+						$makeID = $row_makemst[$b]['makeID'];
+						$makeName = $row_makemst[$b]['makeName'];
+						$equipments = array();
+						$t_total = 0;
+						$t_average = 0;
+						$t_totalParts = 0;
+						$t_totalMisc = 0;
+						$t_totalOth = 0;
+						$noOfVehicles = 0;
+
+						// LOOP YEAR
+						for($a=0;$a<count($row_yearmst);$a++){
+							$yearID = $row_yearmst[$a]['yearID'];
+							$yearDesc = $row_yearmst[$a]['description'];			
+
+							// LOOP MODEL
+							for($c=0;$c<count($row_modelmst);$c++){
+								$modelID = $row_modelmst[$c]['modelID'];
+								$modelDesc = $row_modelmst[$c]['description'];
+								$eCnt = 0;
+								$total = 0;
+								$totalParts = 0;
+								$totalMisc = 0;
+								$totalLabor = 0;
+								$totalPMS = 0;
+								$totalAC = 0;
+								$totalElec = 0;
+								$totalMech = 0;
+								$totalBody = 0;
+								$totalDetailing = 0;
+								$totalRP = 0;
+								$totalOth = 0;
+
+								// LOOP WORK ORDER MASTER
+								for($z=0;$z<count($row_womst);$z++){
+									$woRefNoMst = $row_womst[$z]['woReferenceNo'];
+									$woYearID = $row_womst[$z]['yearID'];
+									$woMakeID = $row_womst[$z]['makeID'];
+									$woModelID = $row_womst[$z]['modelID'];
+									$totalCost = $row_womst[$z]['totalCost'];
+									$parts = $row_womst[$z]['parts'];
+									$miscellaneous = $row_womst[$z]['miscellaneous'];
+									$labor = $row_womst[$z]['labor'];
+									$tax = $row_womst[$z]['tax'];
+									$discount = $row_womst[$z]['discount'];
+									$serviceTypeID = $row_womst[$z]['serviceTypeID'];
+
+									// IF YEAR,MAKE,MODEL EXISTS ON WOMST
+									if($yearID == $woYearID && $makeID == $woMakeID && $modelID == $woModelID){
+										$eCnt++;
+										$total += $totalCost;
+										$totalParts += $parts;
+										$totalMisc += $miscellaneous;
+										$totalLabor += (($labor + $tax) - $discount);
+
+										// COUNT TOTAL OF EACH SERVICE TYPE
+										switch($serviceTypeID){
+											case "ST002": $totalPMS += $totalCost; break;
+											case "ST003": $totalAC += $totalCost; break;
+											case "ST004": $totalElec += $totalCost; break;
+											case "ST005": $totalMech += $totalCost; break;
+											case "ST006": $totalBody += $totalCost; break;
+											case "ST007": $totalDetailing += $totalCost; break;
+											case "ST008": $totalRP += $totalCost; break;
+											case "ST009": $totalOth += $totalCost; break;
+										}
+									}
+								}
+								// END WORK ORDER MST
+
+								if($eCnt > 0){
+									$average = ($total / $eCnt);
+									$t_total += $total;
+									$t_average += $average;
+									$t_totalParts += $totalParts;
+									$t_totalMisc += $totalMisc;
+									$t_totalLabor += $totalLabor;
+									$t_totalPMS += $totalPMS;
+									$t_totalAC += $totalAC;
+									$t_totalElec += $totalElec;
+									$t_totalMech += $totalMech;
+									$t_totalBody += $totalBody;
+									$t_totalDetailing += $totalDetailing;
+									$t_totalRP += $totalRP;
+									$t_totalOth += $totalOth;
+
+									$equipments[] = array("eYearID" => $yearID
+														,"eYearDesc" => $yearDesc
+														,"eMakeID" => $makeID
+														,"eMakeName" => $makeName
+														,"eModelID" => $modelID
+														,"eModelDesc" => $modelDesc
+														,"eVehicles" => $eCnt
+														,"total" => $total
+														,"average" => $average
+														,"parts" => $totalParts
+														,"miscellaneous" => $totalMisc
+														,"labor" => $totalLabor
+														,"PMS" => $totalPMS
+														,"AC" => $totalAC
+														,"elec" => $totalElec
+														,"mech" => $totalMech
+														,"body" => $totalBody
+														,"detailing" => $totalDetailing
+														,"RP" => $totalRP
+														,"others" => $totalOth);
+
+									// COUNT NO OF VEHICLES
+									$noOfVehicles += $eCnt;
+								}
+							}
+							// END MODEL
+						}
+						// END YEAR
+
+						if(count($equipments) > 0){
+							$rowData1[] = array("makeID" => $makeID
+											,"makeName" => $makeName
+											,"equipments" => $equipments
+											,"noOfVehicles" => $noOfVehicles
+											,"total" => $t_total
+											,"average" => $t_average
+											,"parts" => $t_totalParts
+											,"miscellaneous" => $t_totalMisc
+											,"labor" => $t_totalLabor
+											,"PMS" => $t_totalPMS
+											,"AC" => $t_totalAC
+											,"elec" => $t_totalElec
+											,"mech" => $t_totalMech
+											,"body" => $t_totalBody
+											,"detailing" => $t_totalDetailing
+											,"RP" => $t_totalRP
+											,"others" => $t_totalOth);
+						}
+					}
+					// END MAKE
+					
+					$row = $rowData1;
+
+					$ln .= "REPAIR COST PER MAKE REPORT\r\n\r\n";
+
+					$ln .= "From: ," . dateFormat($fromdt,"m/d/Y") . "\r\n";
+					$ln .= "To: ," . dateFormat($todt,"m/d/Y") . "\r\n";
+
+					$ln .= "\r\nYear | Make | Model,,Total (Average),Parts,Oil/Lubricants,Others,Sevice Type\r\n";
+					$ln .= ",,,,,,PMS,A/C,Elec,Mech,Boyd,Detailing,RP,Others\r\n";
+
+					$cnt = 1;
+					for($i=0;$i<count($row);$i++){
+						$ln .= $row[$i]['makeName'] . "\r\n";
+
+						$equipments = $row[$i]['equipments'];
+						for($a=0;$a<count($equipments);$a++){
+							$ln .= $cnt . ".) " . $equipments[$a]['eYearDesc']
+								. " " . $equipments[$a]['eMakeName']
+								. " " . $equipments[$a]['eModelDesc']
+								. "," . $equipments[$a]['eVehicles']
+								. "," . $equipments[$a]['total'] . " (" . $equipments[$a]['average'] . ") "
+								. "," . $equipments[$a]['parts']
+								. "," . $equipments[$a]['miscellaneous']
+								. "," . $equipments[$a]['labor']
+								. "," . $equipments[$a]['PMS']
+								. "," . $equipments[$a]['AC']
+								. "," . $equipments[$a]['elec']
+								. "," . $equipments[$a]['mech']
+								. "," . $equipments[$a]['body']
+								. "," . $equipments[$a]['detailing']
+								. "," . $equipments[$a]['RP']
+								. "," . $equipments[$a]['others']
+								. "\r\n";
+
+							$cnt++;
+						}
+
+						$ln .= "Total >>>>>"
+							. "," . $row[$i]['noOfVehicles']
+							. "," . $row[$i]['total'] . " (" . $row[$i]['average'] . ") "
+							. "," . $row[$i]['parts']
+							. "," . $row[$i]['miscellaneous']
+							. "," . $row[$i]['labor']
+							. "," . $row[$i]['PMS']
+							. "," . $row[$i]['AC']
+							. "," . $row[$i]['elec']
+							. "," . $row[$i]['mech']
+							. "," . $row[$i]['body']
+							. "," . $row[$i]['detailing']
+							. "," . $row[$i]['RP']
+							. "," . $row[$i]['others']
+							. "\r\n";
+					}
+
+					$data = trim($ln);
+					$filename = "tba_cost_per_make_" . $dt . ".csv";
+				break;
+			case "repaircostpersupplier":
+					$fromdt = date("Y-m-d 00:00");
+					$todt = date("Y-m-d 23:59");
+					
+					if(!empty($_POST['txtFromDt'])){
+						$fromdt = dateFormat($_POST['txtFromDt'], "Y-m-d 00:00");
+					}
+
+					if(!empty($_POST['txtToDt'])){
+						$todt = dateFormat($_POST['txtToDt'], "Y-m-d 23:59");
+					}
+
+					// SET FMS DB
+					$fms_db = new DBConfig;
+					$fms_db->setFleetDB();
+
+					// SET WOMST
+					$womst = new Table;
+					$womst->setSQLType($fms_db->getSQLType());
+					$womst->setInstance($fms_db->getInstance());
+					$womst->setView("v_workordermaster");
+					$womst->setParam("WHERE status = '6' AND (invoiceDate BETWEEN '$fromdt' AND '$todt') ORDER BY departmentName");
+					$womst->doQuery("query");
+					$row_womst = $womst->getLists();
+
+					// SET SUPPLIER
+					$suppliermst = new Table;
+					$suppliermst->setSQLType($fms_db->getSQLType());
+					$suppliermst->setInstance($fms_db->getInstance());
+					$suppliermst->setView("v_suppliermaster");
+					$suppliermst->setParam("WHERE status = '1' ORDER BY supplierName");
+					$suppliermst->doQuery("query");
+					$row_suppliermst = $suppliermst->getLists();
+
+					// CLOSING FMS DB
+					$fms_db->DBClose();
+
+					$t_total = 0;
+					$t_average = 0;
+					$t_totalParts = 0;
+					$t_totalMisc = 0;
+					$t_totalOth = 0;
+
+					// LOOP SUPPLIER
+					for($b=0;$b<count($row_suppliermst);$b++){
+						$supplierID = $row_suppliermst[$b]['supplierID'];
+						$supplierName = $row_suppliermst[$b]['supplierName'];
+						$eCnt = 0;
+
+						$total = 0;
+						$totalParts = 0;
+						$totalMisc = 0;
+						$totalLabor = 0;
+						$totalPMS = 0;
+						$totalAC = 0;
+						$totalElec = 0;
+						$totalMech = 0;
+						$totalBody = 0;
+						$totalDetailing = 0;
+						$totalRP = 0;
+						$totalOth = 0;
+
+						// LOOP WORK ORDER MASTER
+						for($z=0;$z<count($row_womst);$z++){
+							$woRefNoMst = $row_womst[$z]['woReferenceNo'];
+							$woSupplierID = $row_womst[$z]['supplierID'];
+							$totalCost = $row_womst[$z]['totalCost'];
+							$parts = $row_womst[$z]['parts'];
+							$miscellaneous = $row_womst[$z]['miscellaneous'];
+							$labor = $row_womst[$z]['labor'];
+							$tax = $row_womst[$z]['tax'];
+							$discount = $row_womst[$z]['discount'];
+							$serviceTypeID = $row_womst[$z]['serviceTypeID'];
+
+							// IF YEAR,MAKE,MODEL EXISTS ON WOMST
+							if($supplierID == $woSupplierID){
+								$eCnt++;
+								$total += $totalCost;
+								$totalParts += $parts;
+								$totalMisc += $miscellaneous;
+								$totalLabor += (($labor + $tax) - $discount);
+
+								// COUNT TOTAL OF EACH SERVICE TYPE
+								switch($serviceTypeID){
+									case "ST002": $totalPMS += $totalCost; break;
+									case "ST003": $totalAC += $totalCost; break;
+									case "ST004": $totalElec += $totalCost; break;
+									case "ST005": $totalMech += $totalCost; break;
+									case "ST006": $totalBody += $totalCost; break;
+									case "ST007": $totalDetailing += $totalCost; break;
+									case "ST008": $totalRP += $totalCost; break;
+									case "ST009": $totalOth += $totalCost; break;
+								}
+							}
+						}
+						// END WORK ORDER MST
+
+						if($eCnt > 0){
+							$average = ($total / $eCnt);
+							$t_total += $total;
+							$t_average += $average;
+							$t_totalParts += $totalParts;
+							$t_totalMisc += $totalMisc;
+							$t_totalLabor += $totalLabor;
+							$t_totalPMS += $totalPMS;
+							$t_totalAC += $totalAC;
+							$t_totalElec += $totalElec;
+							$t_totalMech += $totalMech;
+							$t_totalBody += $totalBody;
+							$t_totalDetailing += $totalDetailing;
+							$t_totalRP += $totalRP;
+							$t_totalOth += $totalOth;
+
+							$rowData1[] = array("supplierID" => $supplierID
+											,"supplierName" => $supplierName
+											,"total" => $t_total
+											,"average" => $t_average
+											,"parts" => $t_totalParts
+											,"miscellaneous" => $t_totalMisc
+											,"labor" => $t_totalLabor
+											,"PMS" => $t_totalPMS
+											,"AC" => $t_totalAC
+											,"elec" => $t_totalElec
+											,"mech" => $t_totalMech
+											,"body" => $t_totalBody
+											,"detailing" => $t_totalDetailing
+											,"RP" => $t_totalRP
+											,"others" => $t_totalOth);
+						}
+						
+						// OVERALL TOTAL
+						$gTotal += $t_total;
+					}
+					// END SUPPLIER
+					
+					$row = $rowData1;
+
+					$ln .= "REPAIR COST PER SUPPLIER REPORT\r\n\r\n";
+
+					$ln .= "From: ," . dateFormat($fromdt,"m/d/Y") . "\r\n";
+					$ln .= "To: ," . dateFormat($todt,"m/d/Y") . "\r\n";
+
+					$ln .= "\r\nSupplier,Total (Average),Parts,Oil/Lubricants,Others,Sevice Type\r\n";
+					$ln .= ",,,,,PMS,A/C,Elec,Mech,Body,Detailing,RP,Others\r\n";
+
+					$cnt = 1; 
+                    $grandTotal = 0;
+                    $t_services = 0;
+                    $t_parts = 0;
+                    $t_misc = 0;
+                    $t_labor = 0;
+                    $t_pms = 0;
+                    $t_ac = 0;
+                    $t_elec = 0;
+                    $t_mech = 0;
+                    $t_body = 0;
+                    $t_detailing = 0;
+                    $t_rp = 0;
+                    $t_others = 0;
+					for($i=0;$i<count($row);$i++){
+						$grandTotal += $row[$i]['total'];
+                        $t_parts += $row[$i]['parts'];
+                        $t_misc += $row[$i]['miscellaneous'];
+                        $t_labor += $row[$i]['labor'];
+                        $t_pms += $row[$i]['PMS'];
+                        $t_ac += $row[$i]['AC'];
+                        $t_elec += $row[$i]['elec'];
+                        $t_mech += $row[$i]['mech'];
+                        $t_body += $row[$i]['body'];
+                        $t_detailing += $row[$i]['detailing'];
+                        $t_rp += $row[$i]['RP'];
+                        $t_others += $row[$i]['others'];
+
+						$ln .= $cnt . ".) " . $row[$i]['supplierName']
+							. "," . $row[$i]['total'] . " (" . $row[$i]['average'] . ") "
+							. "," . $row[$i]['parts']
+							. "," . $row[$i]['miscellaneous']
+							. "," . $row[$i]['labor']
+							. "," . $row[$i]['PMS']
+							. "," . $row[$i]['AC']
+							. "," . $row[$i]['elec']
+							. "," . $row[$i]['mech']
+							. "," . $row[$i]['body']
+							. "," . $row[$i]['detailing']
+							. "," . $row[$i]['RP']
+							. "," . $row[$i]['others']
+							. "\r\n";
+						$cnt++;
+					}
+
+					$noOfSuppliers = ($cnt - 1);
+
+					$ln .= "\r\nGRAND TOTAL: ," . $grandTotal . ",," . "SERVICES \r\n";
+					$ln .= "No. Of Suppliers: ," . $noOfSuppliers . ",PMS: ," . $t_pms . ",Body: ," . $t_body . "\r\n";
+					$ln .= "services: ,,A/C: ," . $t_ac . ",Detailing: ," . $t_detailing . "\r\n";
+					$ln .= "Parts: ," . $t_parts . ",Elec: ," . $t_elec . ",RP: ," . $t_rp . "\r\n";
+					$ln .= "Oil/Lubs: ," . $t_misc . ",Mech: ," . $t_mech . ",Others: ," . $t_others . "\r\n";
+
+					$data = trim($ln);
+					$filename = "repair_cost_per_supplier_" . $dt . ".csv";
 				break;
 			default: break;
 		}
